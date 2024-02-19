183b9f5090dfd1c2b19be3139c746881f02b1287
AkiVoziKavasaki <lazar2757@gmail.com>
2024-02-19 12:13:50 +0100

    something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let question;
+    export let correctAnswer;
+    export let incorrectAnswers;
+
+    
+
+    const shuffle = (array) => array.sort( () => .5 - Math.random() );
+    let possibleAnswers = shuffle([...incorrectAnswers,correctAnswer]);
+
+    const handleClick = (answer) =>{
+        if(answer == correctAnswer){
+            alert("Tacno!")
+        }
+        else{
+            alert(`Netačno! Tačan odgovor je ${correctAnswer}`)
+        }
+    }
+
+</script>
+
+<div>
+    <h3>{@html question}</h3>
+    <div id = "answers">
+        {#each possibleAnswers as pa}
+            <button on:click= {() => handleClick(pa)}>{pa}</button>
+        {/each}
+    </div>
+</div>
---
+++ src/routes/Question.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let question;
+    export let correctAnswer;
+    export let incorrectAnswer;
+
+    let shuffle = (array) => array.sort(() => Math.random() - 0.5);
+    let possibleAnswers = suffle([...incorrectAnswer,correctAnswer]);
+    const handleClick = (answer) => {
+        if (answer == correctAnswer){
+            alert(`Tacno!`)
+        }
+        else{
+            alert(`Netacno! Tacan odgovor je ${correctAnswer} `)
+        }
+    }
+    </script>
+
+    <div>
+        <h3>{@html question}</h3>
+        <div id="answer">
+            <!-- Prikazi sve ponudjene odgovore kao button-e -->
+            {#each possibleAnswers as pa, i}
+                <button on:click={handleClick(i)}>{pa}</button>
+            {/each}
+        </div>
+    </div>
